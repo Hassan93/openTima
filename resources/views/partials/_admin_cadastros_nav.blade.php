224734a6863e70fb4a89e33edd9d4e3600fda5f0
Hassan <hassan@localhost.localdomain>
2017-04-30 15:06:57 +0200

    nav vertcal admin

--- resources/views/partials/_admin_cadastros_nav.blade.php
+++ resources/views/partials/_admin_cadastros_nav.blade.php
@@ -1,13 +1,20 @@
 <div class="ui secondary vertical pointing menu">
-  <a href="{{route('departamentos.index')}}" class="{{Request::is('departamentos' || 'cadastros') ? "active item": ""}}">
-    Departamentos
-  </a>
-  <a href="{{route('cursos.index')}}" class="{{Request::is('cursos' || 'cursos.edit') ? "active item": ""}}">
-    Cursos
-  </a>
+  <div class="element">
+    <a href="{{route('departamentos.index')}}" class="{{Request::is('departamentos') ? "active item": ""}}">
+        Departamentos
+    </a>
+  </div>
+  <div class="element">
+    <a href="{{route('cursos.index')}}" class="{{Request::is('cursos') ? "active item": ""}}">
+      Cursos
+    </a>
+  </div>
+<div class="element">
   <a href="{{route('docentes.index')}}" class="{{Request::is('docentes') ? "active item": ""}}">
     Docentes
   </a>
+</div>
+
   <a class="item">
     Estudantes
   </a>
